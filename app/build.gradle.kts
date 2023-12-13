af672a7bc0ce6691472cfa48a1f329b19dba659f
melchongx <melfredfonclara@gmail.com>
2023-12-13 11:41:31 +0800

    + layout adjustments + call function //favorites func doesn't seem to work, di nassave

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 
 dependencies {
 
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
@@ -53,5 +54,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
 
 }
--- app/src/main/java/com/example/contactmanager/NewContactActivity.kt
+++ app/src/main/java/com/example/contactmanager/NewContactActivity.kt
@@ -1,11 +1,17 @@
 package com.example.contactmanager
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.contactmanager.databinding.ActivityNewContactBinding
 import com.google.firebase.database.FirebaseDatabase
+import java.io.File
 import java.util.regex.Pattern
 
 class NewContactActivity : AppCompatActivity() {
@@ -19,10 +25,42 @@ class NewContactActivity : AppCompatActivity() {
         // get device UUID
         val sharedPreferences = getSharedPreferences("DEVICE_SETTINGS", MODE_PRIVATE)
         val deviceId = sharedPreferences.getString("device_id", "")
+        val SELECT_PICTURE = 200
 
         binding.ibBackButton.setOnClickListener {
             val mainActivityIntent = Intent(this, MainActivity::class.java)
             this.startActivity(mainActivityIntent)
+        }
+
+        val img: ImageView = findViewById(R.id.ivNewContactAvatar)
+
+        fun getFileSize(uri: Uri): Long {
+            val file = File(uri.path)
+            return file.length()
+        }
+
+        val pickImageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK && result.data != null) {
+                val selectedImageUri: Uri? = result.data?.data
+
+                selectedImageUri?.let {uri ->
+                    val fileSize = getFileSize(uri)
+                    val maxSizeBytes = 5 * 1024 * 1024 //5MB
+
+                    if (fileSize <= maxSizeBytes) {
+                        Glide.with(this).load(uri).into(img)
+                    } else {
+                        Toast.makeText(this, "Choose an image that is less than 5MB", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        binding.ivNewContactAvatar.setOnClickListener {
+            val getImageIntent = Intent(Intent.ACTION_PICK)
+            getImageIntent.type = "image/*"
+
+            pickImageLauncher.launch(getImageIntent)
         }
 
         binding.ibCreateContactButton.setOnClickListener {
